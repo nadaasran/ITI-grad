4dd5a45d6b5ec4a0a1da2eae481398d3055f052f
manar-98 <mannarabdo88@gmail.com>
2025-03-19 02:38:41 +0200

    remove nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,12 +2,7 @@
     <nav class="navbar">
         navbbar
       <div class="logo"> Qera'a</div>
-      <ul>
-        <li><NuxtLink to="/">home</NuxtLink></li>
-        <li><NuxtLink to="/books">books</NuxtLink></li>
-        <li><NuxtLink to="/plans">plans</NuxtLink></li>
-        <li><NuxtLink to="/contact">contact</NuxtLink></li>
-      </ul>
+
     </nav>
   </template>
   
