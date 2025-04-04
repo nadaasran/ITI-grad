1e04670073dc0f8407ce37c378e50419ad6c131f
Mostafa Mohamed Ibraheem Ahmed <199245366+nourhan-younis@users.noreply.github.com>
2025-04-04 18:19:16 +0200

    fix conflict

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,9 @@
-<script setup>
-definePageMeta({
-    layout : 'default',
-    layout : 'default'
-})
-</script>
+
 
 <template>
-<<<<<<< HEAD
    content of page
-   <bookcard />
-=======
-   <h1>HOME</h1>
->>>>>>> 909915205c36f198d5f5366f980a07d8033beebf
+  
   </template>
-   <script setup>
-   import bookcard from '@/components/bookcard.vue'
-   </script>
+
   
   
