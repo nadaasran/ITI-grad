46e605283a4a2808815cb053c6d68848e27d7b92
Mostafa Mohamed Ibraheem Ahmed <199245366+nourhan-younis@users.noreply.github.com>
2025-04-12 19:42:55 +0200

    reviews

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="text-black mb-5">
-        <img class="w-24" src="https://images.pexels.com/photos/8820928/pexels-photo-8820928.jpeg?auto=compress&cs=tinysrgb&w=400" alt="">
-        <h4 class="font-bold">Feel The Nature</h4>
-        <p class="font-light text-[#4E3629]">Novel</p>
-        <div class="flex gap-5">
-            <p>$3.88</p>
+        <img class="w-44" src="/images/books.png" alt="">
+        <div class="flex items-center gap-8 mt-3">
+            <h4 class="font-bold font-['Montserrat']">Feel The Nature</h4>
+            <i class="fa-regular fa-heart"></i>
+        </div>
+        <p class="font-light text-[#4E3629] font-['Montserrat']">Novel</p>
+        <div class="flex gap-5 font-['Montserrat']">
+            <p class="font-medium text-[#16100B]">$3.88</p>
             <p class="font-light">Addie Renee</p>
         </div>
         <p>⭐⭐⭐⭐⭐</p>
-        <button class="bg-[#4E3629] w-40 rounded-full text-white">View Book</button>
+        <button class="bg-[#4E3629] w-40 rounded-full text-[#FED8B1]">View Book</button>
     </div>
 </template>
